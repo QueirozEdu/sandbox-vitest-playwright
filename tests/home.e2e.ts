cd06f44268c5fc830f666874aae7adbe8f45a584
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-21 19:35:12 -0300

    new tests

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -1,16 +1,54 @@
-import { test, expect } from '@playwright/test';
+import { makeTestTodoRepository } from '@/core/__tests__/utils/make-test-todo-repository';
+import { test, expect, Page } from '@playwright/test';
+
+const HOME_URL = '/';
+const HEADING = 'Task list';
+const INPUT = 'Task';
+const BUTTON = 'Create Task';
+const BUTTON_BUSY = 'Creating Task...';
+const NEW_TODO_TEXT = 'New Todo';
+
+const getHeading = (p: Page) => p.getByRole('heading', { name: HEADING });
+const getInput = (p: Page) => p.getByRole('textbox', { name: INPUT });
+const getBtn = (p: Page) => p.getByRole('button', { name: BUTTON });
+const getBtnBusy = (p: Page) => p.getByRole('button', { name: BUTTON_BUSY });
+
+const getAll = (p: Page) => ({
+  heading: getHeading(p),
+  input: getInput(p),
+  btn: getBtn(p),
+  btnBusy: getBtnBusy(p),
+});
+
+test.beforeEach(async ({ page }) => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+
+  await page.goto(HOME_URL);
+});
+
+test.afterAll(async () => {
+  const { deleteTodoNoWhere } = await makeTestTodoRepository();
+  await deleteTodoNoWhere();
+});
 
 test.describe('<Home /> (E2E)', () => {
-  // Renderização
-  test.describe('Renderização', () => {
-    test('deve ter o title html correto', async ({ page }) => {
-      await page.goto('/');
+  // Rendering
+  test.describe('Rendering', () => {
+    test('should have the correct html titlr', async ({ page }) => {
+      await expect(page).toHaveTitle('Tests with Vitest and Playwright');
+    });
 
-      await page.waitForTimeout(5000);
+    test('should render the header, input and button to create TODOs', async ({
+      page,
+    }) => {
+      await expect(getHeading(page)).toBeVisible();
+      await expect(getInput(page)).toBeVisible();
+      await expect(getBtn(page)).toBeVisible();
     });
   });
 
   // Create
   // Delete
-  // Errors
+  // Erros
 });
