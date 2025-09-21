8d17a3867f3fde8273d7ab55e2203a588be97e6f
Eduardo Queiroz <eduhmq@gmail.com>
2025-09-21 19:43:47 -0300

    add create todo test

--- tests/home.e2e.ts
+++ tests/home.e2e.ts
@@ -33,7 +33,6 @@ test.afterAll(async () => {
 });
 
 test.describe('<Home /> (E2E)', () => {
-  // Rendering
   test.describe('Rendering', () => {
     test('should have the correct html titlr', async ({ page }) => {
       await expect(page).toHaveTitle('Tests with Vitest and Playwright');
@@ -49,6 +48,59 @@ test.describe('<Home /> (E2E)', () => {
   });
 
   // Create
+  test.describe('Create', () => {
+    test('should allow user to create a todo', async ({ page }) => {
+      const { btn, input } = getAll(page);
+
+      await input.fill(NEW_TODO_TEXT);
+      await btn.click();
+
+      const createdTodo = page
+        .getByRole('listitem')
+        .filter({ hasText: NEW_TODO_TEXT });
+
+      await expect(createdTodo).toBeVisible();
+    });
+
+    test('should trim the input description when creating a TODO', async ({
+      page,
+    }) => {
+      const { btn, input } = getAll(page);
+
+      const textToBeTrimmed = '   no spaces here   ';
+      const textTrimmed = textToBeTrimmed.trim();
+
+      await input.fill(textToBeTrimmed);
+      await btn.click();
+
+      const createdTodo = page
+        .getByRole('listitem')
+        .filter({ hasText: textTrimmed });
+      const createdTodoText = await createdTodo.textContent();
+
+      await expect(createdTodoText).toBe(textTrimmed);
+    });
+
+    test('should allow user to create more than one TODO', async ({ page }) => {
+      const { btn, input } = getAll(page);
+
+      const todo1 = 'Todo 1';
+      const todo2 = 'Todo 2';
+
+      await input.fill(todo1);
+      await btn.click();
+
+      const todo1Item = page.getByRole('listitem').filter({ hasText: todo1 });
+      await expect(todo1Item).toBeVisible();
+
+      await input.fill(todo2);
+      await btn.click();
+
+      const todo2Item = page.getByRole('listitem').filter({ hasText: todo2 });
+      await expect(todo2Item).toBeVisible();
+    });
+  });
+
   // Delete
   // Erros
 });
